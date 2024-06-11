44a3466eb82844c604bf361f75e9e8cba4c3f802
G384032023 <238403@st.takushoku-u.ac.jp>
2024-06-12 03:01:36 +0900

    課題

--- weather.js
+++ weather.js
@@ -127,7 +127,7 @@ function Result(resp){
     re.insertAdjacentElement('beforeend', la);
     checkcount = checkcount + 1;
   }
-  if(checkcount < 1){
+  if(!checked){
     let non = document.createElement('p');
     non.textContent = '項目が選択されていません';
     re.insertAdjacentElement('beforeend', non);
